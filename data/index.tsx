b10c3512ea6d3cc9671f5ca33aa73511906c0e43
shuvamk <kshuvam564@gmail.com>
2022-12-01 02:54:32 +0530

    Explain few concepts

---
+++ data/index.tsx
@@ -0,0 +1,157 @@
+export const CONCEPTS = [
+  {
+    id: 1,
+    term: "Browser",
+    explain: (
+      <div>
+        Browser is a special program on your computer that helps you find and
+        look at websites. Websites are like big online books that have lots of
+        different stories and pictures. Browsers look for websites on the
+        Internet so you can look at them. The Internet is like a big library
+        that has lots of books in it, and the browser helps you find the one you
+        want. When you want to look at a website, you type its address into the
+        browser. This is like finding a book in a library. You can also ask the
+        browser to show you websites about whatever you want. To do this, you
+        type some words into the browser that describe what you're looking for.
+        Then the browser will show you a list of websites that might have what
+        you're looking for. You can then pick one of the websites to look at.
+        Browsers also help you save websites so you can look at them later. They
+        also help you remember your passwords, so you don't have to type them in
+        every time you want to look at a website. Browsers also help keep you
+        safe. They can block websites that might have bad things in them or
+        things that could hurt your computer. That's what a browser is, and it's
+        really helpful for looking at websites on the Internet.
+      </div>
+    ),
+  },
+  {
+    id: 2,
+    term: "useContext hook",
+    explain: (
+      <div>
+        The useContext hook is a special type of hook in React that helps you
+        share information between different parts of your app. It's like a
+        little bridge that lets different parts of your app talk to each other.
+        Imagine you have a game where you want to keep track of your score. You
+        can use the useContext hook to create a “score bridge” so that your
+        score is shared between all the different parts of the game. For
+        example, if you get a point in one part of the game, it will instantly
+        show up in all the other parts of the game. The useContext hook is like
+        a box that stores information. You can tell it what kind of information
+        it should store, like a score or a list of names, and then it will keep
+        track of that information. You can also tell it to change the
+        information, like if you want to add or subtract points from the score.
+        To use the useContext hook, you need to create a context object. This
+        object is like a box that you can tell your app to store information in.
+        You can also tell the context object to change the information when
+        needed. Once you have a context object, you can use the useContext hook
+        to access the information stored in the box. This is the bridge between
+        different parts of your app. You can use the information from the
+        context object to show the score in one part of the game, and you can
+        use the same information to change the score in a different part of the
+        game. So when you're playing your game and you score a point, the
+        useContext hook is like a bridge that instantly updates the score in all
+        the other parts of the game. Pretty cool, huh?
+      </div>
+    ),
+  },
+  {
+    id: 3,
+    term: "redux",
+    explain: (
+      <div>
+        Redux is a way of organizing data in a computer program. It helps keep
+        track of all the pieces of information in the program and makes sure
+        that each piece is in the correct place. Imagine that a computer program
+        is like a big house, with lots of rooms and objects in each room. Redux
+        helps to make sure that each room is in the correct place and that all
+        the objects in the house are organised. Redux works by creating a place
+        to store data from the computer program. This place is called a “store”.
+        In the store, all the pieces of information from the program can be
+        stored, like the type of furniture in each room. Redux helps to make
+        sure that the right type of furniture is in the right place. When the
+        program needs to do something with the data, like move a piece of
+        furniture from one room to another, it sends an “action” to the store.
+        The action tells Redux what needs to be done with the data. Redux then
+        figures out how to do the action, like how to move the furniture, and
+        makes sure that the data is organized correctly. Redux helps to make
+        sure that the data in the program is always organized correctly and that
+        the right pieces of information are in the right place. That way, the
+        program can work properly and do all the things it needs to do.
+      </div>
+    ),
+  },
+  {
+    id: 4,
+    term: "HTML",
+    explain: (
+      <div>
+        HTML stands for HyperText Markup Language. It's a special code that is
+        used to create websites and webpages. As a 5 year old, you may not know
+        what that means, so let me explain. A webpage is like a book. It is made
+        up of words and images that tell a story. HTML is like a special
+        language that is used to tell the computer what the words and images
+        should be and how they should look. It's like a set of instructions that
+        tell the computer what to do. For example, HTML can tell the computer to
+        make the words on the page big or small. It can also tell the computer
+        to add a picture or a video to a page. HTML can even help the computer
+        to know where to put the links on a page. HTML is like a magic code that
+        helps the computer know what to do. Without it, you would never be able
+        to look at a webpage or read a website. HTML is like a secret language,
+        and it helps the computer make all the amazing things that you can find
+        on the internet.
+      </div>
+    ),
+  },
+  {
+    id: 5,
+    term: "CSS",
+    explain: (
+      <div>
+        CSS stands for "Cascading Style Sheets". It is a style sheet language
+        used to describe how a website looks. It is used to make websites look
+        more attractive and to make them easier to use. CSS helps create the
+        look and feel of a website. It can tell the website how to display
+        images, how to make text look, how to position things on the page, and
+        more. It can also improve the usability of a website by making it easier
+        to navigate and find what you're looking for. Think of CSS like a
+        coloring book. You can decide which colors you want to use and where you
+        want to use them. In the same way, you can choose what fonts and colors
+        you want to use on your website and where you want them to go. It's also
+        like building blocks. You can use blocks of code to build a website,
+        like a tower. The blocks are like the building blocks and you can use
+        them to create a great looking website. Finally, CSS is also like an
+        orchestra. You can choose which instruments to use and where to put
+        them. Each instrument is like a piece of code and together they can
+        create a sound that's beautiful. So, CSS is how we make websites look
+        and work great!
+      </div>
+    ),
+  },
+  {
+    id: 6,
+    term: "Javascript",
+    explain: (
+      <div>
+        Javascript is a type of computer language that helps people make
+        websites on the internet. It tells the computer how to show words,
+        images, and movies on the page, and what to do when someone clicks on or
+        types something. Think of it like this: when you play a board game, the
+        cards and pieces tell you what to do and how to move. Javascript works
+        the same way, but it's like an invisible game-master that tells the
+        computer what to do when someone visits a website. Javascript also helps
+        websites do things like remember who you are when you come back, or show
+        different pictures depending on the time of day. Because Javascript
+        works so fast, it can do things like check if you spelled something
+        wrong before you submit it, or show you a special message if it's your
+        birthday. Without Javascript, websites would be much harder to use. For
+        a five-year-old, the best way to explain Javascript would be to use an
+        everyday example. For instance, when you play with your toys, you tell
+        the toys what to do, like to fly or run, and they do it. Javascript
+        works in a similar way; it tells the computer what to do, like show
+        words and pictures or save information, and then the computer does it.
+        It's like a magical language that helps websites work the way they do.
+      </div>
+    ),
+  },
+];
